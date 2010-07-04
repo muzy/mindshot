5ddcde621dce75d600a1d85dc49c17b43bd7e2f5
Sebastian Muszytowski <muzy@muzybot.de>
2010-07-04 12:52:46 +0200

    updated README; added prefs.js

---
+++ extensions/prefs.js
@@ -0,0 +1,23 @@
+user_pref("print.printer_browsershot.print_bgcolor", false);
+user_pref("print.printer_browsershot.print_bgimages", false);
+user_pref("print.printer_browsershot.print_downloadfonts", true);
+user_pref("print.printer_browsershot.print_edge_bottom", 0);
+user_pref("print.printer_browsershot.print_edge_left", 0);
+user_pref("print.printer_browsershot.print_edge_right", 0);
+user_pref("print.printer_browsershot.print_edge_top", 0);
+user_pref("print.printer_browsershot.print_footercenter", "");
+user_pref("print.printer_browsershot.print_footerleft", "");
+user_pref("print.printer_browsershot.print_footerright", "");
+user_pref("print.printer_browsershot.print_headercenter", "");
+user_pref("print.printer_browsershot.print_headerleft", "");
+user_pref("print.printer_browsershot.print_headerright", "");
+user_pref("print.printer_browsershot.print_in_color", true);
+user_pref("print.printer_browsershot.print_margin_bottom", "0");
+user_pref("print.printer_browsershot.print_margin_left", "0");
+user_pref("print.printer_browsershot.print_margin_right", "0");
+user_pref("print.printer_browsershot.print_margin_top", "0");
+user_pref("print.printer_browsershot.print_unwritable_margin_bottom", 0);
+user_pref("print.printer_browsershot.print_unwritable_margin_left", 0);
+user_pref("print.printer_browsershot.print_unwritable_margin_right", 0);
+user_pref("print.printer_browsershot.print_unwritable_margin_top", 0);
+
